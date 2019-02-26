ed7daea8a79b9b252b0a6669270b317037fa2045
dloreto <dloreto.work@gmail.com>
2019-02-26 05:39:25 +0100

    move files

---
+++ L45KeyHoldHook/KeyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L45SpecialCharWinEnhance.L45KeyHoldHook
+{
+    public class KeyInfo
+    {
+        public string KeyCode { get; }
+        public string Key { get; }
+        public bool IsAlphaNumeric { get; }
+        public bool IsCapsLocked { get; }
+
+        public KeyInfo(string keyCode, string key, bool isAlphaNumeric, bool isCapsLocked) {
+            KeyCode = keyCode;
+            Key = key;
+            IsAlphaNumeric = isAlphaNumeric;
+            IsCapsLocked = isCapsLocked;
+        }
+    }
+}
---
+++ L45KeyHoldHook/KeyboardInputUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L45SpecialCharWinEnhance
+{
+    class KeyboardInputUtils
+    {
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(uint virtualKeyCode, uint scanCode,
+        byte[] keyboardState,
+        [Out, MarshalAs(UnmanagedType.LPWStr, SizeConst = 64)]
+        StringBuilder receivingBuffer,
+        int bufferSize, uint flags);
+
+        public static string GetCharsFromKeys(Keys keys, uint scanCode, bool shift, bool ctrl, bool alt, bool isExtended)
+        {
+            var buf = new StringBuilder(256);
+            var keyboardState = new byte[256];
+            if (shift)
+                keyboardState[(int)Keys.ShiftKey] = 0xff;
+            if (ctrl)
+            {
+                keyboardState[(int)Keys.ControlKey] = 0xff;
+            }
+            if (alt)
+            {
+                keyboardState[(int)Keys.Menu] = 0xff;
+            }
+            ToUnicode((uint)keys, scanCode, keyboardState, buf, 256, (uint)(isExtended ? 1 : 0));
+            return buf.ToString();
+        }
+
+        public static Boolean isAlphaNumeric(string strToCheck)
+        {
+            if (String.IsNullOrEmpty(strToCheck))
+            {
+                return false;
+            }
+
+            Regex rg = new Regex(@"^[a-zA-Z0-9\s,]*$");
+            return rg.IsMatch(strToCheck);
+        }
+    }
+}
